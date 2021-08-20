d4bcf3609662ab6fbac6b7b83f03c1e2c49820fd
aesakof <aesakof@gmail.com>
2021-08-19 21:48:14 -0400

    Created Material UI tables for Car and Fillup components

--- react-frontend/src/components/Cars.js
+++ react-frontend/src/components/Cars.js
@@ -1,29 +1,72 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
 
 function Cars() {
-    const [types, setTypes] = useState([])
+    const [cars, setCars] = useState(null)
+    const classes = useStyles();
 
-    // useEffect(() => {
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             for(let i = 0; i < data.types.length; i++) {
-    //                 setTypes(prevTypes => {
-    //                     const t = data.types[i].type.name
-    //                     return [...prevTypes, t]
-    //                 })
-    //             }
-    //             setPokemonInfo(data)
-    //         })
-    // }, [])
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/cars/')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data[0])
+                setCars(data)
+                
+            })
+    }, [])
 
     return (
         <>
-           
+        {
+            cars === null ?
+            <h5>Loading cars data...</h5> :
+
+            <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Car Name</TableCell>
+                    <TableCell>Make</TableCell>
+                    <TableCell>Model</TableCell>
+                    <TableCell>Model Year</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Username</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {cars.map((car) => (
+                    <TableRow key={car.name}>
+                    <TableCell component="th" scope="row">
+                        {car.name}
+                    </TableCell>
+                    <TableCell>{car.make}</TableCell>
+                    <TableCell>{car.model}</TableCell>
+                    <TableCell>{car.model_year}</TableCell>
+                    <TableCell>{car.status}</TableCell>
+                    <TableCell>{car.username}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+        }
         </>
-    )
+      );
 }
 
 export default Cars
--- react-frontend/src/components/Fillups.js
+++ react-frontend/src/components/Fillups.js
@@ -1,29 +1,76 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
 
 function Fillups() {
-    const [types, setTypes] = useState([])
+    const [fillups, setFillups] = useState(null)
+    const classes = useStyles();
 
-    // useEffect(() => {
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             for(let i = 0; i < data.types.length; i++) {
-    //                 setTypes(prevTypes => {
-    //                     const t = data.types[i].type.name
-    //                     return [...prevTypes, t]
-    //                 })
-    //             }
-    //             setPokemonInfo(data)
-    //         })
-    // }, [])
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/fillups/')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data[0])
+                setFillups(data)
+                
+            })
+    }, [])
 
     return (
         <>
-           
+        {
+            fillups === null ?
+            <h5>Loading fillups data...</h5> :
+
+            <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Price Per Gallon</TableCell>
+                    <TableCell>Trip Distance</TableCell>
+                    <TableCell>Gallons</TableCell>
+                    <TableCell>Car</TableCell>
+                    <TableCell>Total Sale</TableCell>
+                    <TableCell>MPG</TableCell>
+                    <TableCell>Username</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {fillups.map((fillup) => (
+                    <TableRow key={fillup.date}>
+                    <TableCell component="th" scope="row">
+                        {fillup.date}
+                    </TableCell>
+                    <TableCell>{fillup.price_per_gallon}</TableCell>
+                    <TableCell>{fillup.trip_distance}</TableCell>
+                    <TableCell>{fillup.gallons}</TableCell>
+                    <TableCell>{fillup.car}</TableCell>
+                    <TableCell>{fillup.total_sale}</TableCell>
+                    <TableCell>{fillup.mpg}</TableCell>
+                    <TableCell>{fillup.username}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+        }
         </>
-    )
+      );
 }
 
 export default Fillups
